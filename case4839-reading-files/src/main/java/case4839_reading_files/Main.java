3743f9aaf4ad20f40536301be527bb5ec05af3e3
Sergey Kubatko <skubatko@gmail.com>
2019-08-21 16:53:35 +0300

    case4839 completed

---
+++ case4839-reading-files/src/main/java/case4839_reading_files/Main.java
@@ -0,0 +1,22 @@
+package case4839_reading_files;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("/Users/TeSSerA/Documents/3. Personal/2. Projects/ИТ/4. Projects/3. Java/2. Java Core/1. Overall/hyperskill/case-study/case4839-reading-files/src/main/java/case4839_reading_files/dataset_91065.txt");
+		Scanner sc = new Scanner(file);
+		int counter = 0;
+		int num = 0;
+		while (sc.hasNextInt() && (num = sc.nextInt()) != 0) {
+			if (num % 2 == 0) {
+				counter++;
+			}
+		}
+		System.out.println(counter);
+	}
+
+}
